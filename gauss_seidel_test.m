3033985bf5fe5ec524f2a7ead6f8930f332135f3
Phillip Lane <iamaperson620@gmail.com>
2022-10-26 19:56:54 -0500

    added gauss-seidel method

---
+++ gauss_seidel_test.m
@@ -0,0 +1,15 @@
+matrixSize = 4;
+
+A = [-2 1 0 0 ; 1 -2 1 0 ; 0 1 -2 1 ; 0 0 1 -2];
+
+b = rand(matrixSize, 1);
+
+[x, iters, residual] = gauss_seidel(A, b, 100, 0.000001);
+fprintf("Took %d iterations\n", iters);
+fprintf("Residual is %d\n", residual);
+disp("A:");
+disp(A);
+disp("x:");
+disp(x);
+disp("Solution");
+disp(A \ b);
